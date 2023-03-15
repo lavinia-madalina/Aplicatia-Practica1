36d4b2c5c3ecb84f1580b45363df0baeef66d4df
lavinia-madalina <mada.burlacu@student.usv.ro>
2023-03-15 03:53:19 +0200

    update

--- Aplicatia practica/Game.cs
+++ Aplicatia practica/Game.cs
@@ -14,6 +14,7 @@ namespace TriviaGame
         {
             Console.WriteLine("Bine ati venit la joculet");
             Console.WriteLine("Press oriunde ca sa iesi");
+            Console.WriteLine("Press oriunde ca sa iesi");
             Console.ReadKey();
         }
     }
