db714105e4d56b0cc81c363591ee7ae200607187
lavinia-madalina <mada.burlacu@student.usv.ro>
2023-03-15 03:50:18 +0200

    Add project files.

---
+++ Aplicatia practica/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriviaGame
+{
+    class Game
+    {
+        public Game()
+        { 
+        
+        }
+        public void Play()
+        {
+            Console.WriteLine("Bine ati venit la joculet");
+            Console.WriteLine("Press oriunde ca sa iesi");
+            Console.ReadKey();
+        }
+    }
+}
